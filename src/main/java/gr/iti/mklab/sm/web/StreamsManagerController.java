8c2c14e6245685a93d89b78a72bb5ad77d6a17d2
manosetro <manosetro@iti.gr>
2016-11-07 17:28:46 +0200

    Fix API of stream manager

--- src/main/java/gr/iti/mklab/sm/web/StreamsManagerController.java
+++ src/main/java/gr/iti/mklab/sm/web/StreamsManagerController.java
@@ -1,18 +1,12 @@
 package gr.iti.mklab.sm.web;
 
-import com.google.gson.Gson;
-import com.mongodb.MongoClient;
-
 import gr.iti.mklab.simmo.core.morphia.MorphiaManager;
 import gr.iti.mklab.sm.StreamsManager;
-import gr.iti.mklab.sm.feeds.Feed;
 import gr.iti.mklab.sm.feeds.GeoFeed;
 import gr.iti.mklab.sm.feeds.KeywordsFeed;
 import gr.iti.mklab.sm.streams.StreamException;
 import gr.iti.mklab.sm.streams.StreamsManagerConfiguration;
 
-import org.mongodb.morphia.Morphia;
-import org.mongodb.morphia.dao.BasicDAO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,14 +17,10 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 @Controller
-@RequestMapping("/sm")
+@RequestMapping("/api")
 public class StreamsManagerController {
 
     private StreamsManager manager;
@@ -66,7 +56,13 @@ public class StreamsManagerController {
 
     /**
      * ** keywords feed request
-     * {"keywords":["grexit"],"id":"Flickr#1","since":"Jun 10, 2015 11:32:47 AM","source":"Flickr","label":"tFlickr"}
+     * 	{
+     * 		"keywords":["grexit"],
+     * 		"id":"Flickr#1",
+     * 		"since":"Jun 10, 2015 11:32:47 AM",
+     * 		"source":"Flickr",
+     * 		"label":"tFlickr"
+     * 	}
      */
     @RequestMapping(value = "/feeds/addkeywords", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
     @ResponseBody
@@ -76,7 +72,16 @@ public class StreamsManagerController {
 
     /**
      * ** geo feed request
-     * {"_lon_min":2.282352,"_lat_min":48.837379,"_lon_max":2.394619,"_lat_max":48.891358,"_density":0.0,"id":"Panoramio#1","source":"Panoramio","label":"tPanoramio"}
+     * 	{
+     * 		"_lon_min":2.282352,
+     * 		"_lat_min":48.837379,
+     * 		"_lon_max":2.394619,
+     * 		"_lat_max":48.891358,
+     * 		"_density":0.0,
+     * 		"id":"Panoramio#1",
+     * 		"source":"Panoramio",
+     * 		"label":"tPanoramio"
+     * 	}
      */
     @RequestMapping(value = "/feeds/addgeo", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
     @ResponseBody
@@ -88,7 +93,6 @@ public class StreamsManagerController {
     @RequestMapping(value = "/feeds/delete", method = RequestMethod.GET, produces = "application/json")
     @ResponseBody
     public String deleteFeed(@RequestParam String id) throws Exception {
-
         return manager.deleteFeed(id);
     }
 
@@ -97,43 +101,5 @@ public class StreamsManagerController {
     public Map<String, Object> status() throws Exception {
     	Map<String, Object> status = manager.getStatus();
     	return status;
-    }
-    
-    public static void main(String[] args) throws Exception {
-        Set<String> keywords = new HashSet<>();
-        keywords.add("grexit");
-        Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.MONTH, -1);
-        KeywordsFeed flickr = new KeywordsFeed();
-        flickr.addKeywords(new ArrayList<String>(keywords));
-        flickr.setId("Flickr#1");
-        flickr.setSinceDate(cal.getTime());
-        flickr.setSource("Flickr");
-        flickr.setLabel("tFlickr");
-        //Gson gson = new Gson();
-        //System.out.println(gson.toJson(flickr));
-
-        double lat1 = 48.837379;
-        double lon1 = 2.282352;
-        double lat2 = 48.891358;
-        double lon2 = 2.394619;
-        GeoFeed panoramio = new GeoFeed(lon1, lat1, lon2, lat2);
-        panoramio.setId("Panoramio#1");
-        panoramio.setSource("Panoramio");
-        panoramio.setLabel("tPanoramio");
-        Gson gson = new Gson();
-        System.out.println(gson.toJson(panoramio));
-
-        Morphia morphia = new Morphia();
-        morphia.map(Feed.class);
-
-        MongoClient mongoClient = new MongoClient("127.0.0.1");
-        BasicDAO<Feed, String> feedsDao = new BasicDAO<>(Feed.class, mongoClient, morphia, "defeeds");
-        // ensure capped collections
-        //feedsDao.getDatastore().ensureCaps();
-
-        feedsDao.save(panoramio);
-        feedsDao.save(flickr);
-
     }
 }
